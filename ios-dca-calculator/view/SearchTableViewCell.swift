9c2a4a69120d36ed7aa5d2c171cf99ed88458cd1
elizeurs <13ders@gmail.com>
2021-06-15 19:04:03 -0300

    Add prototype cells

---
+++ ios-dca-calculator/view/SearchTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchTableViewCell.swift
+//  ios-dca-calculator
+//
+//  Created by Elizeu RS on 15/06/21.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var assetNameLabel: UILabel!
+  @IBOutlet weak var assetSymbolLabel: UILabel!
+  @IBOutlet weak var assetTypeLabel: UILabel!
+}
