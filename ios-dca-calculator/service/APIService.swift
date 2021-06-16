ca93db30e86ee2b3cb42adf6dedfd92926c78533
elizeurs <13ders@gmail.com>
2021-06-16 13:50:09 -0300

    Get API keys

---
+++ ios-dca-calculator/service/APIService.swift
@@ -0,0 +1,15 @@
+//
+//  APIService.swift
+//  ios-dca-calculator
+//
+//  Created by Elizeu RS on 16/06/21.
+//
+
+import Foundation
+
+struct APIService {
+  
+  let API_KEY = "KN8XNH79103W3HIH"
+  let API_KEY2 = "OZLTAABH40S1CV08"
+  let API_KEY3 = "W1BDQQPOLJFCZN53"
+}
