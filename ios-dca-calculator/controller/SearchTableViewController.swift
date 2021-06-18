de94fbeed584579a34238ce58edead85ce01f3ca
elizeurs <13ders@gmail.com>
2021-06-18 15:37:24 -0300

    Show animation on search

--- ios-dca-calculator/controller/SearchTableViewController.swift
+++ ios-dca-calculator/controller/SearchTableViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import Combine
+import MBProgressHUD // lottie animation
 
-class SearchTableViewController: UITableViewController {
+class SearchTableViewController: UITableViewController, UIAnimatable {
   
   private enum Mode {
     case onboading
@@ -54,9 +55,9 @@ class SearchTableViewController: UITableViewController {
     $searchQuery
       .debounce(for: .milliseconds(750), scheduler: RunLoop.main)
       .sink { [unowned self] (searchQuery) in
-        
-        
+        showLoadingAnimation()
         self.apiService.fetchSymbolsPublisher(keywords: searchQuery).sink { (completion) in
+          hideLoadingAnimation()
           switch completion {
           case .failure(let error):
             print(error.localizedDescription)
