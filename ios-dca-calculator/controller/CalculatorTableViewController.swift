016edefa6893061b02256749752a1c96074e0475
elizeurs <13ders@gmail.com>
2021-07-09 22:28:21 -0300

    Improve UI

--- ios-dca-calculator/controller/CalculatorTableViewController.swift
+++ ios-dca-calculator/controller/CalculatorTableViewController.swift
@@ -112,7 +112,7 @@ class CalculatorTableViewController: UITableViewController {
       self?.currentValueLabel.backgroundColor = isProfitable ? .themeGreenShade : .themeRedShade
       self?.currentValueLabel.text = result?.currentValue.currencyFormat
       self?.investmentAmountLabel.text = result?.investmentAmount.toCurrencyFormat(hasDecimalPlaces: false)
-      self?.gainLabel.text = result?.gain.toCurrencyFormat(hasDollarSymbol: false, hasDecimalPlaces: false).prefix(withText: gainSymbol)
+      self?.gainLabel.text = result?.gain.toCurrencyFormat(hasDollarSymbol: true, hasDecimalPlaces: false).prefix(withText: gainSymbol)
       self?.yieldLabel.text = result?.yield.percentageFormat.prefix(withText: gainSymbol).addBrackets()
       self?.yieldLabel.textColor = isProfitable ? .systemGreen : .systemRed
       self?.annualReturnLabel.text = result?.annualReturn.percentageFormat
